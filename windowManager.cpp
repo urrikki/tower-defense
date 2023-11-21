7096d308a245b34e96f3cf83520d43bc736bb1bd
ShulingK <117281266+ShulingK@users.noreply.github.com>
2023-11-21 16:56:43 +0100

    WindowManager

---
+++ windowManager.cpp
@@ -0,0 +1,37 @@
+#include "windowmanager.h"
+#include <iostream>
+#include "gamemanager.h"
+#include "gameobject.h"
+
+// Constructeur
+WindowManager::WindowManager(int _windowWidth, int _windowHeight, const char* _name)
+{
+    windowSize.x = _windowWidth, windowSize.y = _windowHeight;
+    name = _name;
+
+    oWindow = new sf::RenderWindow(sf::VideoMode(windowSize.x, windowSize.y), name);
+}
+
+// Destructeur
+WindowManager::~WindowManager()
+{
+    oWindow->close();
+}
+
+
+// Methods
+
+sf::RenderWindow& WindowManager::GetRenderWindow()
+{
+    return *oWindow;
+}
+
+const sf::Vector2i& WindowManager::GetWindowSize()
+{
+    return windowSize;
+}
+
+void WindowManager::Draw()
+{
+    
+}
--- windowManager.h
+++ windowManager.h
@@ -1,1 +1,40 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/Mouse.hpp>
+#include <vector>
+
+#define DEFAULT_WIDTH 1080
+#define DEFAULT_HEIGHT 720
+
+class GameObject;
+
+class WindowManager
+{
+private:
+    const char* name;
+    sf::RenderWindow* oWindow;
+    sf::Vector2i windowSize;
+
+    WindowManager(int _windowWidth = DEFAULT_WIDTH, int _windowHeight = DEFAULT_HEIGHT, const char* name = "Class Brick");
+
+public:
+    static WindowManager& getInstance()
+    {
+        static WindowManager instance;
+        return instance;
+    }
+
+    WindowManager(const WindowManager&) = delete;
+    WindowManager& operator=(const WindowManager&) = delete;
+
+    ~WindowManager();
+
+    void Update();
+
+    sf::RenderWindow& GetRenderWindow();
+
+    const sf::Vector2i& GetWindowSize();
+
+    void Draw();
+};
